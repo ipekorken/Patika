f522bfd81eab02ea9a292a3acb7cb3b41870ac89
ipekorkene <orkenipek@gmail.com>
2022-02-07 11:44:06 +0300

    template engine

--- NODEJS/PCATProject/app.js
+++ NODEJS/PCATProject/app.js
@@ -1,25 +1,28 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log("Middleware Log 1");
-  next();
-};
-
-const myLogger2 = (req, res, next) => {
-  console.log("Middleware Log 2");
-  next();
-};
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+// express'e template engine olarak ejs kullanacağımızı söylüyoruz.
+// ejs, klasör yapımızdaki views klasörünün içerisine bakar. temp klasörünü views olarak değiştirdik.
 
 //MIDDLEWARES
 app.use(express.static("public")); //express.static => a middleware function in express.
-app.use(myLogger);
-app.use(myLogger2);
 
+//ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
 });
 
 const port = 3000;
