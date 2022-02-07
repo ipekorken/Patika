ebbe048890cb212a52141bda0e4edcb301729cf3
ipekorkene <orkenipek@gmail.com>
2022-02-07 09:58:36 +0300

    add cleanblog

---
+++ NODEJS/CleanBlog/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const photo = {
+    id: 1,
+    title: "Blog Title",
+    description: "Blog description",
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
