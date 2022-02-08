c1421b6f3c90df6213547b485a60cf311bc67026
ipekorkene <orkenipek@gmail.com>
2022-02-08 14:48:59 +0300

    post islemleri

--- NODEJS/PCATProject/app.js
+++ NODEJS/PCATProject/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const path = require("path");
+const Photo = require("./models/Photo");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -16,8 +23,11 @@ app.use(express.urlencoded({ extended: true })); // => aldığımız requesti so
 app.use(express.json()); // => aldığımız requesti sonlandırmamıza yardımcı oldu. url'deki datayı json formatına dönüştürmemizi sağlar.
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -28,10 +38,12 @@ app.get("/add", (req, res) => {
   res.render("add");
 });
 
-app.post("/photos", (req, res) => {
-  console.log(req.body);
+app.post("/photos", async (req, res) => {
+  //console.log(req.body);
+  await Photo.create(req.body); //req.body 'i veritabanına gönderecek.
   res.redirect("/"); // tekrar anasayfaya dönelim dedik.
 });
+// fonksiyonu async yazarak asenkron yaptık böylece model yardımıyla veritabınında oluşturduğumuz döküman olana kadar await durumda bekleyecek.
 
 const port = 3000;
 app.listen(port, () => {
